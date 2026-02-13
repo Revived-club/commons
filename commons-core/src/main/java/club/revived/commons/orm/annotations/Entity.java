3201eadc897d098449c7bfe5c107a91d11eff87f
yyuh <dustinohlemann@gmail.com>
2026-02-13 23:20:23 +0100

    default save method for entity interface

--- commons-core/src/main/java/club/revived/commons/orm/annotations/Entity.java
+++ commons-core/src/main/java/club/revived/commons/orm/annotations/Entity.java
@@ -1,5 +1,11 @@
 package club.revived.commons.orm.annotations;
 
+import club.revived.commons.data.DataRepository;
+
 public interface Entity {
+
+  default void save() {
+    DataRepository.getInstance().save(this.getClass(), this);
+  }
 
 }
