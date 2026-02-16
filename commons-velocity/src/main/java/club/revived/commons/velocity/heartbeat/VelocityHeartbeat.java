2d5e55f918afe808cceae015d497121b89a4b058
yyuhontop <dustinohlemann@gmail.com>
2026-02-16 09:11:03 +0100

    fix velocity heartbeat

--- commons-velocity/src/main/java/club/revived/commons/velocity/heartbeat/VelocityHeartbeat.java
+++ commons-velocity/src/main/java/club/revived/commons/velocity/heartbeat/VelocityHeartbeat.java
@@ -10,37 +10,47 @@ import club.revived.commons.distribution.Cluster;
 import club.revived.commons.distribution.heartbeat.Heartbeat;
 import club.revived.commons.distribution.heartbeat.HeartbeatService;
 import club.revived.commons.distribution.kvbus.providers.broker.MessageBroker;
+import club.revived.commons.distribution.service.ServiceSpecifics;
 
 public final class VelocityHeartbeat extends HeartbeatService {
 
   private final MessageBroker broker;
   private final Cluster cluster;
   private final ProxyServer proxyServer;
+  private final ServiceSpecifics specifics;
 
-  public VelocityHeartbeat(final Cluster cluster, final ProxyServer proxyServer) {
+  public VelocityHeartbeat(final Cluster cluster, final ProxyServer proxyServer, final ServiceSpecifics specifics) {
     this.cluster = cluster;
     this.broker = cluster.getBroker();
     this.proxyServer = proxyServer;
+    this.specifics = specifics;
   }
 
   @Override
   public ScheduledFuture<?> startTask() {
     return super.subServer.scheduleAtFixedRate(() -> {
+      final var specificsBuilder = this.specifics.toBuilder();
+
+      final var onlinePlayers = this.proxyServer.getAllPlayers()
+          .stream()
+          .map(player -> {
+            return new OnlinePlayer(
+                player.getUniqueId(),
+                player.getUsername(),
+                this.cluster.getServiceId(),
+                "",
+                "",
+                (int) player.getPing());
+          }).toList();
+
+      specificsBuilder.onlinePlayers(onlinePlayers);
+
       this.broker.publish("service:heartbeat", new Heartbeat(
           System.currentTimeMillis(),
           cluster.getServiceType(),
           cluster.getServiceId(),
-          proxyServer.getAllPlayers().size(),
-          proxyServer.getAllPlayers().stream()
-              .map(player -> new OnlinePlayer(
-                  player.getUniqueId(),
-                  player.getUsername(),
-                  this.cluster.getServiceId(),
-                  "",
-                  "",
-                  (int) player.getPing()))
-              .toList(),
-          cluster.getIp()));
+          cluster.getIp(),
+          specificsBuilder.build()));
     }, 0, HeartbeatService.INTERVAL, TimeUnit.MILLISECONDS);
   }
 }
