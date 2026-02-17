9368049f106d03f202c0b9a3be20377a8cb62e3b
yyuh <dustinohlemann@gmail.com>
2026-02-17 18:30:14 +0100

    profile manager

--- commons-core/src/main/java/club/revived/commons/game/player/ProfileManager.java
+++ commons-core/src/main/java/club/revived/commons/game/player/ProfileManager.java
@@ -9,30 +9,30 @@ import java.util.concurrent.CompletableFuture;
 
 public final class ProfileManager {
 
-    private  static  ProfileManager instance;
+  private static ProfileManager instance;
 
-    @NotNull
-    public CompletableFuture<Void> saveProfile(final PlayerProfile profile) {
-        return DataRepository.getInstance().save(PlayerProfile.class, profile);
+  @NotNull
+  public CompletableFuture<Void> saveProfile(final PlayerProfile profile) {
+    return DataRepository.getInstance().save(PlayerProfile.class, profile);
+  }
+
+  @NotNull
+  public CompletableFuture<Optional<PlayerProfile>> getProfile(final UUID uuid) {
+    return DataRepository.getInstance().get(PlayerProfile.class, uuid);
+  }
+
+  @NotNull
+  public CompletableFuture<Optional<PlayerProfile>> getProfile(final String name) {
+    return DataRepository.getInstance().getByField(PlayerProfile.class, "name", name)
+        .thenApply(list -> list.isEmpty() ? Optional.empty() : Optional.of(list.getFirst()));
+  }
+
+  public static ProfileManager getInstance() {
+    if (instance == null) {
+      instance = new ProfileManager();
+      return instance;
     }
 
-    @NotNull
-    public CompletableFuture<Optional<PlayerProfile>> getProfile(final UUID uuid) {
-        return DataRepository.getInstance().get(PlayerProfile.class, uuid);
-    }
-
-    @NotNull
-    public CompletableFuture<Optional<PlayerProfile>> getProfile(final String name) {
-        return DataRepository.getInstance().getByField(PlayerProfile.class, "name", name)
-                .thenApply(list -> list.isEmpty() ? Optional.empty() : Optional.of(list.getFirst()));
-    }
-
-    public  static  ProfileManager getInstance() {
-        if (instance == null) {
-            instance = new ProfileManager();
-            return  instance;
-        }
-
-        return  instance;
-    }
+    return instance;
+  }
 }
